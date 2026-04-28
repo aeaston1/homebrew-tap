b29d60365865fd9e222558b23e4df8c438823031
aeaston1 <9115196+aeaston1@users.noreply.github.com>
2026-04-28 22:30:34 +0200

    Set cupld formula version metadata

--- Formula/cupld.rb
+++ Formula/cupld.rb
@@ -1,18 +1,22 @@
 class Cupld < Formula
   desc "Local graph database CLI and REPL"
   homepage "https://github.com/aeaston1/cupld"
+
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/aeaston1/cupld/releases/download/v0.2.0/cupld-aarch64-apple-darwin.tar.xz"
+    url "https://github.com/aeaston1/cupld/releases/download/v0.2.0/cupld-aarch64-apple-darwin.tar.xz",
+        tag: "v0.2.0"
     sha256 "d8f00ef6d5142a22d7633cecd69e7c6f9c2752e39f7f0c5e27d50efbb0e4d016"
   end
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/aeaston1/cupld/releases/download/v0.2.0/cupld-x86_64-apple-darwin.tar.xz"
+    url "https://github.com/aeaston1/cupld/releases/download/v0.2.0/cupld-x86_64-apple-darwin.tar.xz",
+        tag: "v0.2.0"
     sha256 "d4485ac42164f2946dda204ee7dbfba65365a17a37e88fd2d3f3091fdb3ddd9e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/aeaston1/cupld/releases/download/v0.2.0/cupld-x86_64-unknown-linux-musl.tar.xz"
+    url "https://github.com/aeaston1/cupld/releases/download/v0.2.0/cupld-x86_64-unknown-linux-musl.tar.xz",
+        tag: "v0.2.0"
     sha256 "660fd6a92b945aae70f0c2c5acda7483af5367afe85633ff6db7cecdd7589477"
   end
   license "MIT"
